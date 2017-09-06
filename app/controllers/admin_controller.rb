5ff9db74bc893628346c9ebd3d95e9b482834e2b
Freddy Fallon <freddyfallon@gmail.com>
2017-09-06 10:35:40 +0100

    Move views into partials

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,6 @@
 class AdminController < ApplicationController
   def index
-    @first_messages = Message.where(boring_level: 0)
-    @first_replies = Reply.where(tolerance_level: 0)
-    @stage_one_messages = Message.where(stage: 1)
-    @stage_two_messages = Message.where(stage: 2)
-    @stage_three_messages = Message.where(stage: 3)
-    @stage_one_replies = Reply.where(stage: 1)
-    @stage_two_replies = Reply.where(stage: 2)
-    @stage_three_replies = Reply.where(stage: 3)
+
   end
 
 end
